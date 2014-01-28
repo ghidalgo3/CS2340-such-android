27a67cc2a1875a77eea5f1cbcf7012160a9ed8b6
Nathan Braswell <nathan@nathanbraswell.com>
2014-01-27 19:45:38 -0500

    Added a really helpful comment

--- FinancialPlanningApp/FinancialPlanning/src/main/java/edu/gatech/CS2340/suchwow/MainActivity.java
+++ FinancialPlanningApp/FinancialPlanning/src/main/java/edu/gatech/CS2340/suchwow/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.ViewGroup;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
+    //Woo MainActivity, this comment is so useful!!!!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
