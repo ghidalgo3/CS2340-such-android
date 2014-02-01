c0895e0770460fbc3f6b83445c6c02e6f60d66af
Gustavo Hidalgo <zambrano.hidalgo@gmail.com>
2014-01-31 21:08:00 -0500

    Added Log-in Page

--- FinancialPlanningApp/FinancialPlanning/src/main/java/edu/gatech/CS2340/suchwow/MainActivity.java
+++ FinancialPlanningApp/FinancialPlanning/src/main/java/edu/gatech/CS2340/suchwow/MainActivity.java
@@ -1,51 +1,34 @@
 package edu.gatech.CS2340.suchwow;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button login, register;
     //Result of merge!
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        login = (Button)this.findViewById(R.id.login);
+        register = (Button)this.findViewById(R.id.register);
     }
 
 
+    public void loginPressed(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
-
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
     }
 
 }
