b3b1cdd37d38234d12962fb1b8b22ffd1ba9c15a
Nathan Braswell <nathan@nathanbraswell.com>
2014-02-10 22:56:56 -0500

    Registering is implemented now. Note that the command_line_build.gradle doesn't quite work now, I assume because of somthing related to the learning project.

--- FinancialPlanningApp/FinancialPlanning/src/main/java/edu/gatech/CS2340/suchwow/MainActivity.java
+++ FinancialPlanningApp/FinancialPlanning/src/main/java/edu/gatech/CS2340/suchwow/MainActivity.java
@@ -25,6 +25,11 @@ public class MainActivity extends ActionBarActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    public void registerPressed(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
